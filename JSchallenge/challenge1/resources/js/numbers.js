2e0e9c4d9f9e1102af6198254b79fa00ce7569bc
s.sivanantham <sangeetha.sivanantham@gmail.com>
2020-06-15 11:57:11 +0100

    JS challenge1 - Added HTML input

--- JSchallenge/challenge1/resources/js/numbers.js
+++ JSchallenge/challenge1/resources/js/numbers.js
@@ -1,8 +1,6 @@
 
-/*function createTable(){
-let arraylen = document.getElementById("arraylen").value,
-    multiple = document.getElementById("multiple").value;
-*/
+//Method 1 to get the input with prompt
+/*
   let arraylen = prompt("ENTER A NUMBER", "100");
   let multiple = prompt("ENTER A MULTIPLE?", "5");   
 document.write("HERE ARE YOUR NUMBERS TILL " + arraylen + " IN MULTIPLES OF " + multiple + "<br /> <br />");
@@ -33,3 +31,37 @@ window.addEventListener("load", function (){
         // Attach table to container
         document.getElementById("container").appendChild(table);
       });
+
+      */
+
+document.getElementById("btncreatetable").onclick = function() {
+        createTable();
+}
+function createTable(){
+let arraylen = document.getElementById("arraylen").value,
+    multiple = document.getElementById("multiple").value;
+
+        // Draw HTML table
+
+        let table = document.createElement("table"),
+            row = table.insertRow();
+      
+        for (let i=1; i <= arraylen; i++)  
+        {
+            var nextmultiple = i * multiple;
+            if (nextmultiple <= arraylen)
+            {
+            let cell1 = row.insertCell();
+            cell1.innerHTML = i + " * " + multiple + " = "; 
+
+            let cell2 = row.insertCell();
+            cell2.innerHTML = nextmultiple;
+           
+          // Break into next row
+            }
+            row = table.insertRow();
+        }
+      
+        // Attach table to container
+        document.getElementById("container").appendChild(table);
+      };
